0bfa538e8e4069a32ebb21a4b2e01b39ccaa3c9b
Minh03-code <21211tt4504@mail.tdc.edu.vn>
2023-09-27 07:24:43 +0700

    Complete navigation bar

---
+++ app/src/main/java/com/example/mobileproject/activity/admin/AdminActivity.java
@@ -0,0 +1,107 @@
+package com.example.mobileproject.activity.admin;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.mobileproject.R;
+import com.example.mobileproject.fragment.admin.AbstractFragment;
+import com.example.mobileproject.fragment.admin.AppManagementFragment;
+import com.example.mobileproject.fragment.admin.ManagementFragment;
+import com.example.mobileproject.fragment.admin.ProfileFragment;
+import com.example.mobileproject.fragment.admin.WageFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class AdminActivity extends AppCompatActivity {
+
+    private final int MANAGEMENT_SCREEN = 1000;
+    private final int APP_MANAGEMENT_SCREEN = 1001;
+    private final int WAGE_SCREEN = 1002;
+    private final int PROFILE_SCREEN = 1003;
+
+    private int screenID = MANAGEMENT_SCREEN;
+
+
+    AbstractFragment fragment;
+    FragmentTransaction transaction;
+
+
+    private BottomNavigationView bottomNavigationView;
+
+
+    private DrawerLayout drawerLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.drawer_admin_layout);
+
+
+        drawerLayout = findViewById(R.id.idDrawerAdminLayout);
+        getLayoutInflater().inflate(R.layout.main_admin_layout, drawerLayout);
+        replaceFragment();
+        bottomNavigationView = findViewById(R.id.bnvAdmin);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nvaQuanLy:
+                        screenID = MANAGEMENT_SCREEN;
+                        replaceFragment();
+                        break;
+                    case R.id.nvaUngDung:
+                        screenID = APP_MANAGEMENT_SCREEN;
+                        replaceFragment();
+                        break;
+                    case R.id.nvaLuong:
+                        screenID = WAGE_SCREEN;
+                        replaceFragment();
+                        break;
+                    case R.id.nvaTaiKhoan:
+                        screenID = PROFILE_SCREEN;
+                        replaceFragment();
+                        break;
+                }
+                return true;
+            }
+        });
+
+
+
+    }
+
+
+    // replace fragment
+    private void replaceFragment(){
+        if (getSupportFragmentManager().findFragmentByTag(screenID + "") != null) {
+            fragment = (AbstractFragment) getSupportFragmentManager().findFragmentByTag(screenID + "");
+        } else {
+            if (screenID == MANAGEMENT_SCREEN) {
+                fragment = new ManagementFragment();
+            }
+            if (screenID == APP_MANAGEMENT_SCREEN) {
+                fragment = new AppManagementFragment();
+            }
+            if (screenID == WAGE_SCREEN) {
+                fragment = new WageFragment();
+            }
+            if (screenID == PROFILE_SCREEN) {
+                fragment = new ProfileFragment();
+            }
+        }
+
+        if (fragment != null) {
+            transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.idFragmentAdmin, fragment, screenID + "");
+            if (getSupportFragmentManager().findFragmentByTag(screenID + "") == null) {
+                transaction.addToBackStack(screenID + "");
+            }
+            transaction.commit();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/mobileproject/fragment/admin/AppManagementFragment.java
@@ -0,0 +1,23 @@
+package com.example.mobileproject.fragment.admin;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.mobileproject.R;
+
+public class AppManagementFragment extends AbstractFragment{
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View fragmentLayout = null;
+        fragmentLayout = inflater.inflate(R.layout.fragment_admin_app_management_layout, container, false);
+
+
+        return fragmentLayout;
+    }
+}
