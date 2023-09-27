5fc1b88124523688dff54ef58dc09f976310fdd1
gianghiem0310 <jangiim93nt@gmail.com>
2023-09-27 08:31:51 +0700

    giaodien

---
+++ app/src/main/java/com/example/mobileproject/ManHinhDoiMKAdmin.java
@@ -0,0 +1,14 @@
+package com.example.mobileproject;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ManHinhDoiMKAdmin extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.manhinhdoimatkhau);
+    }
+}
