7edf102026bb421f2a882cf8816fa0e318d40d62
Minh03-code <21211tt4504@mail.tdc.edu.vn>
2023-10-01 19:27:36 +0700

    xóa dòng thừa

--- app/src/main/java/com/example/mobileproject/fragment/admin/AppManagementFragment.java
+++ app/src/main/java/com/example/mobileproject/fragment/admin/AppManagementFragment.java
@@ -52,9 +52,6 @@ public class AppManagementFragment extends AbstractFragment{
             }
         }).attach();
 
-        requireActivity().getOnBackPressedDispatcher().addCallback(){
-
-        };
 
 
         return fragmentLayout;
