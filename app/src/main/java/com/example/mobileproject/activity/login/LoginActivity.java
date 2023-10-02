18c3203ccd35709c5dc00a4c94d56b35ed9b2e15
phuc0707 <nugyenhphuc0707@gmail.com>
2023-10-02 18:13:01 +0700

    Giao dien Login, Register

---
+++ app/src/main/java/com/example/mobileproject/activity/login/LoginActivity.java
@@ -0,0 +1,76 @@
+package com.example.mobileproject.activity.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.mobileproject.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText edtUsername, edtPassWord;
+    Button btnLogin, btnRegister;
+
+
+    String userName,passWord;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity_layout);
+
+        edtPassWord = findViewById(R.id.edt_password);
+        edtUsername = findViewById(R.id.edt_username);
+        btnLogin = findViewById(R.id.btn_login);
+        btnRegister = findViewById(R.id.btn_register);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkLogin();
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void checkLogin(){
+        userName = edtUsername.getText().toString();
+        passWord = edtPassWord.getText().toString();
+        if (userName.isEmpty() || passWord.isEmpty()){
+            alertFail("Username and Password is required");
+        }else {
+            sendLogin();
+        }
+    }
+
+    private void sendLogin() {
+       Toast.makeText(this,"Send",Toast.LENGTH_SHORT).show();
+    }
+
+    private void alertFail(String s) {
+        new AlertDialog.Builder(this)
+                .setTitle("Failed")
+                .setIcon(R.drawable.icon_warning)
+                .setMessage(s)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                }).show();
+    }
+}
