7ab86ac9c4020805ba9a23fbc6b2d65bedca22ca
gianghiem0310 <jangiim93nt@gmail.com>
2023-10-06 20:22:14 +0700

    manhinheditchinhsach

--- app/src/main/java/com/example/mobileproject/fragment/admin/ProfileFragment.java
+++ app/src/main/java/com/example/mobileproject/fragment/admin/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.mobileproject.fragment.admin;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,17 +8,31 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatImageView;
 
 import com.example.mobileproject.R;
+import com.example.mobileproject.activity.admin.EditPolicyActivity;
 
 public class ProfileFragment extends AbstractFragment{
+
+    AppCompatImageView nextChinhSach;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View fragmentLayout = null;
         fragmentLayout = inflater.inflate(R.layout.fragment_admin_profile_layout, container, false);
-
+        anhXa(fragmentLayout);
+        nextChinhSach.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), EditPolicyActivity.class);
+                startActivity(intent);
+            }
+        });
 
         return fragmentLayout;
+    }
+    private void anhXa(View fragment){
+        nextChinhSach = fragment.findViewById(R.id.nextDoiChinhSach);
     }
 }
