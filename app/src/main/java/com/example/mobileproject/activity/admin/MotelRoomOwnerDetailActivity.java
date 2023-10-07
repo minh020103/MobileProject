80eb4a63f1cc94e05e3b4c1687a0d9041772aed2
KietDo1 <21211tt2801@mail.tdc.edu.vn>
2023-10-07 15:23:43 +0700

    commit subnet

---
+++ app/src/main/java/com/example/mobileproject/activity/admin/MotelRoomOwnerDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.mobileproject.activity.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MotelRoomOwnerDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_motel_room_owner_detail);
+    }
+}
