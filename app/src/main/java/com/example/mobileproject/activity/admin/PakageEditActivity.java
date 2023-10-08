f86c04477d617d19f6710a1f3a9ed49a27925def
KietDo1 <21211tt2801@mail.tdc.edu.vn>
2023-10-08 21:30:39 +0700

    update project

--- app/src/main/java/com/example/mobileproject/activity/admin/PakageEditActivity.java
+++ app/src/main/java/com/example/mobileproject/activity/admin/PakageEditActivity.java
@@ -4,12 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.example.mobileproject.R;
+import com.example.mobileproject.api.ApiServiceKiet;
+import com.example.mobileproject.appuntil.AppUntil;
+import com.example.mobileproject.datamodel.GoiDichVu;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class PakageEditActivity extends AppCompatActivity {
     ImageView imgBackGoiDVSua;
+    EditText edtThoiHanGoiDVSua;
+    EditText edtSoLuongPhongGoiDVSua;
+    EditText edtGiaGoiDVSua;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +28,35 @@ public class PakageEditActivity extends AppCompatActivity {
         setContentView(R.layout.pakage_edit_layout);
 
         imgBackGoiDVSua = findViewById(R.id.imgBackGoiDVSua);
+        edtThoiHanGoiDVSua = findViewById(R.id.edtThoiHanGoiDVSua);
+        edtSoLuongPhongGoiDVSua = findViewById(R.id.edtSoLuongPhongGoiDVSua);
+        edtGiaGoiDVSua = findViewById(R.id.edtGiaGoiDVSua);
 
         imgBackGoiDVSua.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        PakageByIdAPI(AppUntil.ID_GOI_DICH_VU);
+    }
+
+    private void PakageByIdAPI(int key)
+    {
+        ApiServiceKiet.apiServiceKiet.getPakageByIdAPI(key).enqueue(new Callback<GoiDichVu>() {
+            @Override
+            public void onResponse(Call<GoiDichVu> call, Response<GoiDichVu> response) {
+                GoiDichVu host = response.body();
+
+                edtThoiHanGoiDVSua.setText(String.valueOf(host.getThoiHan()));
+                edtSoLuongPhongGoiDVSua.setText(String.valueOf(host.getSoLuongPhong()));
+                edtGiaGoiDVSua.setText(String.valueOf(host.getGiaGoi()));
+
+            }
+            @Override
+            public void onFailure(Call<GoiDichVu> call, Throwable t) {
+
             }
         });
     }
