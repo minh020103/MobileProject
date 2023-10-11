b8ee1314b5cd08a19c5da8ec82adea7cc8f5cdac
gianghiem0310 <jangiim93nt@gmail.com>
2023-10-11 10:23:38 +0700

    update data

--- app/src/main/java/com/example/mobileproject/api/admin/ApiServiceNghiem.java
+++ app/src/main/java/com/example/mobileproject/api/admin/ApiServiceNghiem.java
@@ -1,12 +1,31 @@
 package com.example.mobileproject.api.admin;
 
+import com.example.mobileproject.api.Const;
+import com.example.mobileproject.datamodel.ChinhSach;
+
+import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
-public class ApiServiceNghiem {
-    static String BASE_URL = "http://192.168.190.1/3t/laravel/public/";
+public interface ApiServiceNghiem {
+
     ApiServiceNghiem apiService =new Retrofit.Builder()
-            .baseUrl(ApiServiceNghiem.BASE_URL)
+            .baseUrl(Const.DOMAIN)
             .addConverterFactory(GsonConverterFactory.create())
             .build().create(ApiServiceNghiem.class);
+
+    @GET("api/chinhsach")
+    Call<ChinhSach> layChinhSachXuong(@Query("id") int id);
+
+    @PUT("api/capnhatchinhsach")
+    Call<Integer> capNhatChinhSach(@Query("id") int id,@Body ChinhSach chinhSach);
+
+
+
+
 }
