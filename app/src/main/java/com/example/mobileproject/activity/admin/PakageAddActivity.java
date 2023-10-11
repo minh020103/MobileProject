f047673778808c8154d557c9220e366ad35b2e83
KietDo1 <21211tt2801@mail.tdc.edu.vn>
2023-10-11 09:49:29 +0700

    update code

--- app/src/main/java/com/example/mobileproject/activity/admin/PakageAddActivity.java
+++ app/src/main/java/com/example/mobileproject/activity/admin/PakageAddActivity.java
@@ -1,4 +1,38 @@
 package com.example.mobileproject.activity.admin;
 
-public class PakageAddActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.example.mobileproject.R;
+
+public class PakageAddActivity extends AppCompatActivity {
+    ImageView imgBackGoiDVThem;
+    EditText edtThoiHanGoiDVThem;
+    EditText edtSoLuongPhongGoiDVThem;
+    EditText edtGiaGoiDVThem;
+    Button btnGoiDVThem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pakage_add_layout);
+
+        imgBackGoiDVThem = findViewById(R.id.imgBackGoiDVThem);
+        edtThoiHanGoiDVThem = findViewById(R.id.edtThoiHanGoiDVThem);
+        edtSoLuongPhongGoiDVThem = findViewById(R.id.edtSoLuongPhongGoiDVThem);
+        edtGiaGoiDVThem = findViewById(R.id.edtGiaGoiDVThem);
+        btnGoiDVThem = findViewById(R.id.btnGoiDVThem);
+
+        imgBackGoiDVThem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
 }
