00db3b898ee7cfa3d4563fa941c9cf28fac1998e
phuc0707 <nguyenhphuc0707@gmail.com>
2023-10-15 01:03:35 +0700

    Commit

---
+++ app/src/main/java/com/example/mobileproject/activity/login/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.example.mobileproject.activity.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mobileproject.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity_layout);
+    }
+}
